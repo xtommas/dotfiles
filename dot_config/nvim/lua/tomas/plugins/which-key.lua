434342cc7f5588d2146181da31a5fc304ef0e136
Tomas <tomascanepa1397@gmail.com>
2024-07-13 19:32:34 -0300

    Open Nvim-tree at the current file location is now <leader>E, due to a change in which-key :(

--- dot_config/nvim/lua/tomas/plugins/which-key.lua
+++ dot_config/nvim/lua/tomas/plugins/which-key.lua
@@ -1,16 +1,17 @@
 return {
 	"folke/which-key.nvim",
-	event = "VimEnter",
-	config = function()
-		require("which-key").setup()
-
-		-- Document existing key chains
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-		})
-	end,
+	event = "VeryLazy",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
